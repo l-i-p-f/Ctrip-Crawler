8ad3e1cbd5f8a36903386a4fbba5d9ae5b6c9440
leeffff <lipf702@gmail.com>
2025-11-10 00:10:22 +0800

    clean code

--- main.py
+++ main.py
@@ -8,37 +8,37 @@ from core.utils import init_driver, generate_flight_routes, generate_flight_date
 def ctrip_crawler_runner():
     driver = init_driver()
 
-    citys = generate_flight_routes(CRAWL_CITYS)
+    cities = generate_flight_routes(CRAWL_CITYS)
 
     flight_dates = generate_flight_dates(CRAWL_DAYS, BEGIN_DATE, END_DATE, START_INTERVAL, DAYS_INTERVAL)
 
-    Flight_DataFetcher = DataFetcher(driver)
+    flight_data_fetcher = DataFetcher(driver)
 
-    for city in citys:
-        Flight_DataFetcher.city = city
+    for city in cities:
+        flight_data_fetcher.city = city
 
         for flight_date in flight_dates:
-            Flight_DataFetcher.date = flight_date
+            flight_data_fetcher.date = flight_date
 
             if os.path.exists(
                     os.path.join(os.getcwd(), flight_date, dt.now().strftime("%Y-%m-%d"), f"{city[0]}-{city[1]}.csv")):
                 print(
                     f'{time.strftime("%Y-%m-%d_%H-%M-%S")} 文件已存在:{os.path.join(os.getcwd(), flight_date, dt.now().strftime("%Y-%m-%d"), f"{city[0]}-{city[1]}.csv")}')
                 continue
-            elif 'http' not in Flight_DataFetcher.driver.current_url:
+            elif 'http' not in flight_data_fetcher.driver.current_url:
                 print(f'{time.strftime("%Y-%m-%d_%H-%M-%S")} 当前的URL是：{driver.current_url}')
                 # 初始化页面
-                Flight_DataFetcher.get_page(1)
+                flight_data_fetcher.get_page(1)
 
             else:
                 # 后续运行只需更换出发与目的地
-                Flight_DataFetcher.change_city()
+                flight_data_fetcher.change_city()
 
             time.sleep(CRAWL_INTERVAL)
 
     # 运行结束退出
     try:
-        driver = Flight_DataFetcher.driver
+        driver = flight_data_fetcher.driver
         driver.quit()
     except Exception as e:
         print(f'{time.strftime("%Y-%m-%d_%H-%M-%S")} An error occurred while quitting the driver: {e}')
